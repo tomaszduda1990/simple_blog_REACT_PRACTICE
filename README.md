8a973534fb2e40fcc7d8b1bceec21f1fab635f31
Tomasz Duda <tomaszduda1990@users.noreply.github.com>
2018-11-13 19:34:22 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# simple_blog_REACT_PRACTICE
-Created with CodeSandbox
---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
+
+class Blog extends Component {
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: null
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Max"
+          };
+        });
+        this.setState({ posts: updatedPosts });
+        // console.log( response );
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState(() => ({ error: true }));
+      });
+  }
+
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
+
+  render() {
+    const posts = this.state.posts.map(post => {
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
+    });
+    const errorMessage = <p>Error has occured... Please try again later</p>;
+    return (
+      <div>
+        <section className="Posts">
+          {this.state.error ? errorMessage : posts}
+        </section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Blog;
